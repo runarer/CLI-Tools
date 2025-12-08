eda89a7787b34c46540269e153fc9c5932a0b823
runarer <95697611+runarer@users.noreply.github.com>
2025-12-08 13:30:25 +0100

    Basic touch

--- touch/Program.cs
+++ touch/Program.cs
@@ -1,7 +1,24 @@
 ﻿if(args.Length == 0)
 {
     Console.WriteLine("Usage: touch filename");
-    return;
+    return 0;
 }
 
 string filename = args[0];
+
+if(File.Exists(filename))
+{
+    Console.WriteLine("File allready Exisit");
+    return 1;
+}
+
+try
+{
+    File.Create(args[0]);
+} catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    return 1;
+}
+
+return 0;
