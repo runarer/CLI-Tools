64ca57e5e9cba65c953a6e6cb21f10e44f3f0f1f
runarer <95697611+runarer@users.noreply.github.com>
2025-12-08 14:58:44 +0100

    More on ls

---
+++ ls/Program.cs
@@ -0,0 +1,28 @@
+﻿string path;
+string searchPattern = "";
+
+if(args.Length == 0)
+{
+    path =  Directory.GetCurrentDirectory();
+}
+else
+{
+    path = args[0];
+}
+
+if(!Directory.Exists(path))
+{
+    // Could be an argument like -l
+    //If file doesn't exist it could be a searchPattern like *.txt
+    searchPattern = args[0];
+    path = Directory.GetCurrentDirectory();
+}
+
+string[] directoryContent = Directory.GetFiles(path,searchPattern);
+
+foreach (string name in directoryContent)
+{
+    Console.WriteLine("- " + name);
+}
+
+return 0;
