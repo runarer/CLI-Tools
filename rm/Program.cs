a16fbfaecadfb25c54712687fdada2e2f4110a8a
runarer <95697611+runarer@users.noreply.github.com>
2025-12-09 10:47:26 +0100

    Delete my recursive method

--- rm/Program.cs
+++ rm/Program.cs
@@ -10,29 +10,6 @@ DeleteListed(content);
 
 return 0;
 
-static List<string> GetDirectoryContent(string path, string searchPattern)
-{
-    List<string> content = [];
-
-    if(File.Exists(path))
-    {
-        content.Add(path);
-    } 
-    else if (Directory.Exists(path))
-    {
-        if(searchPattern == String.Empty) {
-            content.Add(path);
-        }
-        string[] directoriesInPath = Directory.GetDirectories(path);
-        string[] filesInDirectory = Directory.GetFiles(path,searchPattern);
-        foreach(string directory in directoriesInPath) 
-            content.AddRange(GetDirectoryContent(directory,searchPattern));
-        content.AddRange(filesInDirectory);  
-    } 
-
-    return content;
-}
-
 bool ParseArguments(string[] args, Dictionary<string,char> attributes)
 {
 
