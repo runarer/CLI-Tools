a9fbdc13c09c5877d475d595638cf66818eeb126
runarer <95697611+runarer@users.noreply.github.com>
2025-12-09 10:27:02 +0100

    Adds rm with print instead of remove

---
+++ rm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+string path = args[0];
+string searchPattern = String.Empty;
+
+if(args.Length == 2)
+    searchPattern = args[1];
+
+var content = GetDirectoryContent(path,searchPattern);
+DeleteListed(content);
+
+return 0;
+
+static List<string> GetDirectoryContent(string path, string searchPattern)
+{
+    List<string> content = [];
+
+    if(File.Exists(path))
+    {
+        content.Add(path);
+    } 
+    else if (Directory.Exists(path))
+    {
+        if(searchPattern == String.Empty) {
+            content.Add(path);
+        }
+        string[] directoriesInPath = Directory.GetDirectories(path);
+        string[] filesInDirectory = Directory.GetFiles(path,searchPattern);
+        foreach(string directory in directoriesInPath) 
+            content.AddRange(GetDirectoryContent(directory,searchPattern));
+        content.AddRange(filesInDirectory);  
+    } 
+
+    return content;
+}
+
+static void DeleteListed(List<string> list)
+{
+    // list.Reverse();
+    foreach(string s in Enumerable.Reverse(list) )
+    {
+        Console.WriteLine(s);
+    }
+}
